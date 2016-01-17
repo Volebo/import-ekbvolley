467bad3a0a7237c86879a379efc47fc2694b1cf3
maxkoryukov <maxkoryukov@yandex.ru>
2016-01-17 06:20:28 +0500

    travis.yml 1 & coverage-calc

--- coverage-calc.sh
+++ coverage-calc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-./node_modules/.bin/istanbul cover _mocha --report lcovonly -- --recursive  -R spec test/*.js
+# ./node_modules/.bin/
+istanbul cover _mocha --report lcovonly -- --recursive  -R spec test/*.js
